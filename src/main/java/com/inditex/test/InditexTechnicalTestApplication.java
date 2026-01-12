433c5818dc1410adcff0156288effd78bd5872a2
Sergio Martín <mafrune3@hotmail.com>
2026-01-12 07:12:17 +0100

    Init project

---
+++ src/main/java/com/inditex/test/InditexTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexTechnicalTestApplication.class, args);
+	}
+
+}
